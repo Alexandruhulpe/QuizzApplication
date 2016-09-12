86e7e3ad10649eade287c21597e8048e631cc5cb
Samuel Walladge <samuel@swalladge.id.au>
2016-09-12 13:41:44 +0930

    add stub methods for taking the quiz and resetting answers

--- app/src/main/java/au/id/swalladge/quiz_android/MainActivity.java
+++ app/src/main/java/au/id/swalladge/quiz_android/MainActivity.java
@@ -2,6 +2,7 @@ package au.id.swalladge.quiz_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 // main activity is where most of the stuff is
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    /**
+     * resets any saved answers
+     * @param view
+     */
+    public void resetAnswers(View view) {
+        // TODO
+        // connect to database/wherever answers are saved
+        // and reset to default
+    }
+
+    /**
+     * runs the quiz! should go straight to first question page
+     * @param view
+     */
+    public void takeQuiz(View view) {
+        // TODO
+        // run the quiz question activity
     }
 }
