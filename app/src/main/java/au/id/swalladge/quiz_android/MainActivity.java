ccadb6ff4613fac7d550d0ee3a3ce833bec736ba
Samuel Walladge <samuel@swalladge.id.au>
2016-09-12 13:29:03 +0930

    set up main activity general structure
    - banner image
    - general layout
    - string resources for text

--- app/src/main/java/au/id/swalladge/quiz_android/MainActivity.java
+++ app/src/main/java/au/id/swalladge/quiz_android/MainActivity.java
@@ -3,11 +3,13 @@ package au.id.swalladge.quiz_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// main activity is where most of the stuff is
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
